af2222d77c1fdd1aa0188c06eef4f5d8348bb1f7
schwar3kat <61094841+schwar3kat@users.noreply.github.com>
2023-06-30 13:56:55 +1200

    Update hexchat-armbian-webscroll-win.cmd
    Fix comment delimiter for windows cmd

--- hexchat-armbian-webscroll-win.cmd
+++ hexchat-armbian-webscroll-win.cmd
@@ -1,10 +1,10 @@
-# Assumes that you have OpenSSH public key based authentication configured on your windows 10 computer
-# Assumes that you have the linux script hexchat-armbian-webscroll.sh in ~/.config/hexchat/scripts/
-# Generate the scrollback text #armbian.txt in ~/.config/hexchat/scrollback/liberachat/
-# Scp copy the linux computer scrollback text #armbian.txt to the windows computer in C:\Programs\HexChat\config\scrollback\LiberaChat\
-# Replace "user@linux_computer" with your own connection details.
-# Replace "~/.config/hexchat/scripts/" with the loction of your script on the Linux computer.
-# Replace "C:\Programs\HexChat\config\scrollback\LiberaChat\#armbian.txt" with the location of your Windows Hexchat scripts directory.
+::# Assumes that you have OpenSSH public key based authentication configured on your windows 10 computer
+::# Assumes that you have the linux script hexchat-armbian-webscroll.sh in ~/.config/hexchat/scripts/
+::# Generate the scrollback text #armbian.txt in ~/.config/hexchat/scrollback/liberachat/
+::# Scp copy the linux computer scrollback text #armbian.txt to the windows computer in C:\Programs\HexChat\config\scrollback\LiberaChat\
+::# Replace "user@linux_computer" with your own connection details.
+::# Replace "~/.config/hexchat/scripts/" with the loction of your script on the Linux computer.
+::# Replace "C:\Programs\HexChat\config\scrollback\LiberaChat\#armbian.txt" with the location of your Windows Hexchat scripts directory.
 
 echo ~/.config/hexchat/scripts/hexchat-armbian-webscroll.sh;exit | ssh -T user@linux_computer
 scp user@linux_computer:~/.config/hexchat/scrollback/liberachat/#armbian.txt C:\Programs\HexChat\config\scrollback\LiberaChat\#armbian.txt
